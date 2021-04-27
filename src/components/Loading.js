040df46f1d361c2706f681726663c2c9dbeb2465
Yididiya <yididiyazike@gmail.com>
2021-04-27 06:56:54 +0300

    added react hooks

---
+++ src/components/Loading.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const styles = {
+    content: {
+        fontSize: '35px',
+        position: 'absolute',
+        left: '0',
+        right: '0',
+        marginTop: '20px',
+        textAlign: 'center',
+    }
+}
+
+export default function Loading({ speed = 300, text = 'Loading' }){
+    const [content, setContent] = React.useState(text)
+
+    React.useEffect(() => {
+        const interval = window.setInterval(() => {
+            setContent(content => {  
+                return text + '...' === content  
+                ? text
+                : content + '.'})
+        }, speed)
+
+        return () => window.clearInterval(interval)
+    }, [speed, text])
+
+    return (
+        <p style={styles.content}>
+            {content}
+        </p>
+    )
+}
+
+Loading.propTypes = {
+    text: PropTypes.string,
+    speed: PropTypes.number
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Loading from './components/Loading'
+
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Results = React.lazy(() => import('./components/Results'))
+
+function App(){
+  const [theme, setTheme] = React.useState('light')
+  const toggleTheme = () => setTheme((theme) => theme === 'light' ? 'dark' : 'light') 
+
+  return (
+    <Router>
+      <ThemeProvider value={theme}>
+        <div className={theme}>
+          <div className='container'>
+              <Nav toggleTheme={toggleTheme}/>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path='/' component={Popular} />
+                  <Route exact path='/battle' component={Battle} />
+                  <Route path='/battle/results' component={Results} />
+                  <Route render={() => <h1>404</h1>} />
+                </Switch>
+              </React.Suspense>
+          </div>
+        </div>
+      </ThemeProvider>
+    </Router>
+  ) 
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
