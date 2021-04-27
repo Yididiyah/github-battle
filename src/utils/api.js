7e8c8dc981390ecca2ce3c8e92e3de2c95fac217
Yididiya <yididiyazike@gmail.com>
2021-04-27 11:15:48 +0300

    Fixed bugs :bug:

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,83 +1,79 @@
-const id = '7dfe178cffe845508a85'
-const sec = 'adc3849597886cb83517a810fd2de124024ca36a'
+const id = "YOUR_CLIENT_ID"
+const sec = "YOUR_SECRET_ID"
 const params = `?client_id=${id}&client_secret=${sec}`
 
-function getErrorMsg (message, username){
-    if(message === 'Not Found'){
-        return `${username} doesn't exist`
+function getErrorMsg (message, username) {
+    if (message === 'Not Found') {
+      return `${username} doesn't exist`
     }
-
+  
     return message
-}
+  }
 
-function getProfile (username){
+function getProfile (username) {
     return fetch(`https://api.github.com/users/${username}${params}`)
-    .then((res) =>  res.json())
-    .then((profile) => {
-        if(profile.message){
-            throw new Error(getErrorMsg(profile.message,username))
+      .then((res) => res.json())
+      .then((profile) => {
+        if (profile.message) {
+          throw new Error(getErrorMsg(profile.message, username))
         }
-
+  
         return profile
-    })
-}
+      })
+  }
 
-function getRepos(username){
+function getRepos (username) {
     return fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
-    .then((res) => res.json())
-    .then((repos) => {
-        if(repos.message){
-            throw new Error(getErrorMsg(repos.message, username))
+      .then((res) => res.json())
+      .then((repos) => {
+        if (repos.message) {
+          throw new Error(getErrorMsg(repos.message, username))
         }
-
+  
         return repos
-    })
-}
+      })
+  }
 
-function getStarCount(repos){
+function getStarCount (repos) {
     return repos.reduce((count, { stargazers_count }) => count + stargazers_count , 0)
-}
-
-function calculateScore(followers, repos){
+  }
+  
+function calculateScore (followers, repos) {
     return (followers * 3) + getStarCount(repos)
-}
-
-function getUserData(player){
+  }
+  
+function getUserData (player) {
     return Promise.all([
-        getProfile(player),
-        getRepos(player)
-    ]).then(([profile, repos]) => ({
-        profile,
-        score: calculateScore(profile.followers, repos)
+      getProfile(player),
+      getRepos(player)
+    ]).then(([ profile, repos ]) => ({
+      profile,
+      score: calculateScore(profile.followers, repos)
     }))
-}
+  }
 
-function sortPlayers(players) {
-    return players.sort((a,b) => b.score - a.score)
-}
-
-export function battle (players){
+function sortPlayers (players) {
+    return players.sort((a, b) => b.score - a.score)
+  }
+  
+export function battle (players) {
     return Promise.all([
       getUserData(players[0]),
       getUserData(players[1])
     ]).then((results) => sortPlayers(results))
 }
 
-export function fetchPopularRepos (language){
+
+export function fetchPopularRepos(language){
     const endpoint = window.encodeURI(`https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`)
 
-    return fetch(endpoint,
-            {
-                headers: {
-                    authorization: 'token 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'
-                }
-            }
-        )
+    return fetch(endpoint)
         .then((res) => res.json())
         .then((data) => {
             if(!data.items){
                 throw new Error(data.message)
             }
+
             return data.items
         })
 }
